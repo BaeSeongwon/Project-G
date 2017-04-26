dc979f5f3a3236dc3a509553c1118de6355a9a9a
DESKTOP-VDM42EC\Life <seongwon9179@gmail.com>
2017-04-26 20:14:31 +0900

    font-awesome 추가 siteAppend 모듈 추가

--- src/js/app.js
+++ src/js/app.js
@@ -5,23 +5,28 @@ require('angular-material');
 /* 미들웨어 호출 */
 import configure from './configure/config.js';
 import DashBoardController from './dashboard/dashboard.controller.js';
+import DashBoardMenueService from './dashboard/dashboard.menue.service.js';
 import LoginController from './login/login.controller.js';
 import LoginService from './login/login.service.js';
 import RegisterController from './register/register.controller.js';
 import RegisterService from './register/register.service.js';
+import SiteAppendController from './siteappend/siteappend.controller.js';
 
 /* 미들웨어 CSS 호출 */
 import AngularMaterial from '../../node_modules/angular-material/angular-material.min.css';
+import FontAwesome from '../../node_modules/font-awesome/css/font-awesome.css';
 import IndexStyle from '../public/css/index.css';
 import DashBoardStyle from '../public/css/dashboard.css';
 import LoginStyle from '../public/css/login.css';
 import RegisterStyle from '../public/css/register.css';
-import
+import SiteAppendStyle from '../public/css/siteappend.css';
 
 angular.module('app',['ui.router','ngMaterial'])
         .config(configure)
         .controller('DashBoardController',DashBoardController)
+        .service('DashBoardMenueService',DashBoardMenueService)
         .controller('LoginController',LoginController)
         .service('LoginService',LoginService)
         .controller('RegisterController',RegisterController)
-        .service('RegisterService',RegisterService);
+        .service('RegisterService',RegisterService)
+        .controller('SiteAppendController',SiteAppendController);
